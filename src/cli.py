db9e965a87830d0d387a80cfbffea0b263f8c725
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-12-04 00:47:47 +0100

    removed file flag

--- src/cli.py
+++ src/cli.py
@@ -5,7 +5,6 @@ import argcomplete
 def get_args():
     parser = argparse.ArgumentParser(description='Convert project files to text')
     parser.add_argument('path', help='Path to the file or directory')
-    parser.add_argument('-f', '--file', action='store_true', help='Process a single file')
     parser.add_argument('-t', '--temp', help='Path to a custom temp folder', default=None)
     parser.add_argument('--ignore', help='Comma-separated list of patterns to ignore', default='')
     argcomplete.autocomplete(parser)
