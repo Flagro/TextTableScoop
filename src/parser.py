b5c399b1934203d0d555c955e7c6a76028d1b040
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-12-04 14:48:39 +0100

    new parser logic

--- src/parser.py
+++ src/parser.py
@@ -1,35 +1,46 @@
-from abc import abstractmethod
-import os
-
-class ParsebleFile:
-    def __init__(self, file_path):
-        self.file_path = file_path
-        self.is_lockable = False
-        self.office_format = False
+from .utils import iterate_directory_files, is_dir
+from .types import get_file_parser
 
 
-class ProjectParser:
-    @abstractmethod
-    def __init__(self, tmp_folder_path):
-        self.tmp_folder_path = tmp_folder_path
+class JSONOutputCollector:
+    def __init__(self):
+        self._output = []
 
-    @abstractmethod
-    def parse(self, path):
-        pass
+    def add(self, file_path, **kwargs):
+        self._output.append(kwargs)
 
-class FolderToFilesParser:
-    def __init__(self, tmp_folder_path):
-        self.tmp_folder_path = tmp_folder_path
+    def get(self):
+        return self._output
 
-    def parse(self, path):
-        pass
 
-class FileToTextParser:
-    def __init__(self, tmp_folder_path):
-        self.tmp_folder_path = tmp_folder_path
+class ProjectTraverser:
+    def __init__(self, ignore_patterns, temp_folder_path, json_output_collector):
+        self._ignore_patterns = ignore_patterns
+        self._temp_folder_path = temp_folder_path
+        self._json_output_collector = json_output_collector
 
-    def parse(self, path):
-        pass
+    def process_directory(self, path, relative_path):
+        for file_path, new_relative_path in iterate_directory_files(path, self._ignore_patterns, relative_path):
+            self.process_file(file_path, new_relative_path)
 
-def process():
-    pass
+    def process_file(self, file_path, relative_path):
+        file_parser = get_file_parser(file_path, relative_path)
+        if file_parser.is_archive():
+            self.process_archive(file_parser, relative_path)
+        else:
+            self._json_output_collector.add(relative_path, file_parser.parse())
+
+    def process_archive(self, file_parser, relative_path):
+        with file_parser.parse() as unpacked_folder_path:
+            self.process_directory(unpacked_folder_path, relative_path)
+
+
+def process(path, temp, ignore):
+    ignore_patterns = [pattern.strip() for pattern in ignore.split(',')] if ignore else []
+    json_output_collector = JSONOutputCollector()
+    project_traverser = ProjectTraverser(ignore_patterns, temp, json_output_collector)
+    if is_dir(path):
+        project_traverser.process_directory(path, "")
+    else:
+        project_traverser.process_file(path, "")
+    return json_output_collector.get()
