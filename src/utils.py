c33747eee89d4e591228d03f67679eb3814670cb
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-12-04 12:57:50 +0100

    modified folders files getter

--- src/utils.py
+++ src/utils.py
@@ -10,11 +10,16 @@ def should_ignore(path, ignore_patterns):
     return any(fnmatch.fnmatch(path, pattern) for pattern in ignore_patterns)
 
 
-def iterate_folders_files(path, ignore_patterns):
+def get_folders_files(path, ignore_patterns):
+    folders_files = []
     for dirpath, dirnames, filenames in os.walk(path):
         # Filter directories and files based on ignore patterns
         dirnames[:] = [d for d in dirnames if not should_ignore(os.path.join(dirpath, d), ignore_patterns)]
         filenames[:] = [f for f in filenames if not should_ignore(os.path.join(dirpath, f), ignore_patterns)]
-
         for filename in filenames:
-            yield (dirpath, filename)
+            folders_files.append(os.path.join(dirpath, filename))
+    return folders_files
+
+
+def is_file(path):
+    return os.path.isfile(path)
