10341fe051ccac14afbfb424c96bb9587f4886b5
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-12-04 14:44:49 +0100

    relative path and get file name

--- src/utils.py
+++ src/utils.py
@@ -6,11 +6,15 @@ def get_file_extension(file_path):
     return os.path.splitext(file_path)[1]
 
 
+def get_file_name(file_path):
+    return os.path.basename(file_path)
+
+
 def should_ignore(path, ignore_patterns):
     return any(fnmatch.fnmatch(path, pattern) for pattern in ignore_patterns)
 
 
-def iterate_directory_files(path, ignore_patterns):
+def iterate_directory_files(path, ignore_patterns, parent_folder_path):
     for dirpath, dirnames, filenames in os.walk(path):
         # Filter directories and files based on ignore patterns
         dirnames[:] = [d for d in dirnames if not should_ignore(os.path.join(dirpath, d), ignore_patterns)]
@@ -19,7 +23,7 @@ def iterate_directory_files(path, ignore_patterns):
         for filename in filenames:
             full_path = os.path.join(dirpath, filename)
             relative_path = os.path.relpath(full_path, start=path)
-            yield (full_path, relative_path)
+            yield (full_path, os.path.join(parent_folder_path, relative_path))
 
 
 def is_file(path):
